3be13a1f4aed37b3708ce63c05e642ecbbcf225f
JeongYeonH <ubitnoa@gmail.com>
2026-03-09 12:55:54 +0900

    조합 문제 풂

--- week2/problem-solving/난이도중_문자열_IPv6_실버1.py
+++ week2/problem-solving/난이도중_문자열_IPv6_실버1.py
@@ -1,2 +1,43 @@
 # 문자열 - IPv6 (백준 실버1)
 # 문제 링크: https://www.acmicpc.net/problem/3107
+
+import sys
+
+
+try:
+    sys.stdin = open('난이도중_문자열_IPv6_실버1.txt', 'r')
+except FileNotFoundError:
+    pass
+
+strings = list(input().split(':'))
+
+if strings[0] == '': 
+    strings = strings[1:]
+if strings[-1] == '': 
+    strings = strings[:-1]
+n = len(strings)
+
+before = 0
+after = 0
+isCounted = False
+for i in range(n):
+    if strings[i] == '':
+        isCounted = True
+        continue
+    
+    strings[i] = str(strings[i]).zfill(4)
+    if not isCounted:
+        before += 1
+    else:
+        after += 1
+
+answer = ''
+repeats = 8 - (before + after)
+for i in range(n):
+    if i == before:
+        answer += "0000:" * repeats
+        continue
+    answer += f"{strings[i]}:"
+# print(f"{before} - {after}")
+# print(strings)
+print(answer[:-1])
--- week2/problem-solving/난이도중_완전탐색_차이를최대로_실버2.py
+++ week2/problem-solving/난이도중_완전탐색_차이를최대로_실버2.py
@@ -1,2 +1,45 @@
 # 완전탐색 - 차이를 최대로 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/10819
+
+import sys
+import math
+
+
+try:
+    sys.stdin = open('난이도중_완전탐색_차이를최대로_실버2.txt', 'r')
+except FileNotFoundError:
+    pass
+
+n = int(input())
+nums = list(map(int, input().split()))
+visited = [False] * n
+temp_list = []
+max_sum = -sys.maxsize
+
+def getSum(list):
+    sum = 0
+    for i in range(1, len(list)):
+        sum += abs(list[i - 1] - list[i])
+    return sum
+
+def recursive():
+    global max_sum
+
+    if len(temp_list) == len(nums):
+        sum = getSum(temp_list)
+        max_sum = max(max_sum, sum)
+        return
+
+    for i in range(len(nums)):
+        if visited[i]:
+            continue
+
+        num = nums[i]
+        temp_list.append(num)
+        visited[i] = True
+        recursive()
+        temp_list.pop()
+        visited[i] = False
+
+recursive()
+print(max_sum)
