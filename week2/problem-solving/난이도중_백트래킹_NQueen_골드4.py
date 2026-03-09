97fa51f321219392b3f5d1a4ab2d5c9ee4654672
JeongYeonH <ubitnoa@gmail.com>
2026-03-10 01:55:22 +0900

    중 수준임에도 버겁네

--- week2/problem-solving/난이도중_백트래킹_NQueen_골드4.py
+++ week2/problem-solving/난이도중_백트래킹_NQueen_골드4.py
@@ -1,2 +1,95 @@
 # 백트래킹 - N-Queen (백준 골드4)
 # 문제 링크: https://www.acmicpc.net/problem/9663
+
+import sys
+import math
+
+
+try:
+    sys.stdin = open('난이도중_백트래킹_NQueen_골드4.txt', 'r')
+except FileNotFoundError:
+    pass
+
+N = int(input())
+
+chosen = set()
+list = [set() for _ in range(N)]
+count = 0
+
+def recursion_back(temp):
+    global count
+
+    if len(temp) == N:
+        print(f"최종: {temp}")
+        count += 1
+        return
+
+    compare_set = list[len(temp)]
+    # print(f"{list} - {temp}")
+    # print(f"비교군: {compare_set} - {chosen}")
+    for i in range(N):
+        # print(f"현재 순회 변수-- {i}")
+        if i in compare_set:
+            # print(f"compare_set에 있음: {i}")
+            continue
+        if i in chosen:
+            # print(f"chosen에 있음: {i}")
+            continue
+
+        setting_set(i, temp)
+        temp.append(i)
+        # print(f"넣는 변수: {i}")
+        # print(f"전반적 지도: {list}")
+        # print(f"-----------")
+        recursion_back(temp)
+        removing_set(i, temp)
+        temp.pop()
+
+
+def setting_set(i, temp):
+    x_pos = len(temp)
+    # print(f"x_pos {x_pos}")
+    chosen.add(i)
+    #아래 대각선
+    for o in range(i, N): 
+        y_pos = x_pos + (o - i)  
+        # print(f"y_pos {y_pos}") 
+        if o >= N or o < 0 or y_pos >= N or y_pos < 0:
+            continue
+        list[y_pos].add(o)
+
+    # 위 대각선
+    for o in range(i, -1, -1): # 2 1 0 순
+        # print(f"o: {o} o - i: {o - i}")
+        y_pos = x_pos - (o - i)   
+        # print(f"y_pos {y_pos}")
+        if o >= N or o < 0 or y_pos >= N or y_pos < 0:
+            continue
+        list[y_pos].add(o)    
+
+def removing_set(i, temp):
+    x_pos = len(temp) - 1
+    # print(f"x_pos {x_pos}")
+    chosen.discard(i)
+    for o in range(i, N):   # 반대로 지우면 됨
+        y_pos = x_pos + (o - i)   
+        if o >= N or o < 0 or y_pos >= N or y_pos < 0:
+            continue
+        # print(o)
+        list[y_pos].discard(o)
+
+    #위 대각선
+    for o in range(i, -1, -1): # 2 1 0 순
+        # print(f"o: {o} o - i: {o - i}")
+        y_pos = x_pos - (o - i)   
+        # print(f"y_pos {y_pos}")
+        if o >= N or o < 0 or y_pos >= N or y_pos < 0:
+            continue
+        list[y_pos].discard(o)  
+
+recursion_back([])
+print(count)
+
+# setting_set(2, [1])
+# removing_set(2, [1, 1])
+# print(f"들어감: {list}")
--- week2/problem-solving/난이도중_정수론_골드바흐의추측_실버2.py
+++ week2/problem-solving/난이도중_정수론_골드바흐의추측_실버2.py
@@ -1,2 +1,47 @@
 # 정수론 - 골드바흐의 추측 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/9020
+
+import sys
+import math
+
+try:
+    sys.stdin = open('난이도중_정수론_골드바흐의추측_실버2.txt', 'r')
+except FileNotFoundError:
+    pass
+
+n = int(input())
+list = []
+max_num = -float('inf')
+add_amout = 1
+
+while True:
+    try:
+        line = int(input())
+        list.append(line)
+        max_num = max(line, max_num)
+    except EOFError:
+        break
+    
+#에라토스테네스의 체
+is_prime = [True] * (max_num + 1)
+is_prime[0] = False
+is_prime[1] = False
+
+for start in range(2, max_num + 1):
+    for num in range(start, max_num + 1, start):
+        if num > start:
+            is_prime[num] = False
+
+def find_prime(num):
+    half = int(num / 2)
+    if is_prime[half]:
+        print(f"{half} {half}")
+    else:
+        for i in range(half + 1):
+            if is_prime[half - i] and is_prime[half + i]:
+                print(f"{half - i} {half + i}")
+                break
+
+for num in list:
+    find_prime(num)
+
