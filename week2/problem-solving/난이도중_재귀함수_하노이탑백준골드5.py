802d7d7ce470074b24cac69270fff0c94e2f1b26
JeongYeonH <ubitnoa@gmail.com>
2026-03-09 20:27:47 +0900

    하노이 탑 - 재귀함수로 품

--- week2/problem-solving/난이도중_재귀함수_하노이탑백준골드5.py
+++ week2/problem-solving/난이도중_재귀함수_하노이탑백준골드5.py
@@ -1,2 +1,31 @@
 # 재귀함수 - 하노이 탑 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/1914
+
+import sys
+
+
+try:
+    sys.stdin = open('난이도중_재귀함수_하노이탑백준골드5.txt', 'r')
+except FileNotFoundError:
+    pass
+
+floors = int(input())
+turn = 2 ** floors - 1
+
+print(turn)
+
+def recursion_move(floors, start, end):
+
+    if floors == 1:
+        print(f"{start} {end}")
+        return
+    
+    mid = 6 - start - end
+
+    recursion_move(floors - 1, start, mid)
+    print(f"{start} {end}")
+    recursion_move(floors - 1, mid, end)
+
+if floors <= 20:
+    recursion_move(floors, 1, 3)
+
