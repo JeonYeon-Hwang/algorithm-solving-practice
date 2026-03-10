e96856b2f0e08000b1be909ab4b13d7694bad9bf
JeongYeonH <ubitnoa@gmail.com>
2026-03-11 02:33:52 +0900

    extra + 1 개 풂

--- week2/problem-solving/Extra_배열_HIndex.py
+++ week2/problem-solving/Extra_배열_HIndex.py
@@ -1,6 +1,9 @@
 # 배열 - H-Index
 # 문제 링크: https://leetcode.com/problems/h-index/?envType=study-plan-v2&envId=top-interview-150
 
+from typing import List
+
+
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
         citations.sort(reverse=True)
