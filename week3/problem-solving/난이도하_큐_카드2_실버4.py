bb85ae2f3451e8bd10df7875e3c351b66b15ccf5
JeongYeonH <ubitnoa@gmail.com>
2026-03-14 01:17:42 +0900

    문제를 제대로 읽고 풀이할 것

--- week3/problem-solving/난이도하_큐_카드2_실버4.py
+++ week3/problem-solving/난이도하_큐_카드2_실버4.py
@@ -1,2 +1,25 @@
 # 큐 - 카드2 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/2164
+import sys
+from collections import deque
+
+try:
+    sys.stdin = open('난이도하_큐_카드2_실버4.txt', 'r')
+except FileNotFoundError:
+    pass
+
+input = sys.stdin.readline
+N = int(input())
+
+queue = deque()
+
+for i in range(1, N + 1):
+    queue.append(i)
+
+while len(queue) > 1:
+    queue.popleft()
+    be_last = queue.popleft()
+    queue.append(be_last)
+
+
+print(queue[0])
