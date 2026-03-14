983429c499fa95f0a814320aeb81e49e0ed01884
JeongYeonH <ubitnoa@gmail.com>
2026-03-14 14:44:56 +0900

    난해하다.

--- week3/problem-solving/난이도중_분할정복_곱셈_실버1.py
+++ week3/problem-solving/난이도중_분할정복_곱셈_실버1.py
@@ -1,2 +1,32 @@
 # 분할정복 - 곱셈 (백준 실버1)
 # 문제 링크: https://www.acmicpc.net/problem/1629
+import sys
+
+
+try:
+    sys.stdin = open('난이도중_분할정복_곱셈_실버1.txt', 'r')
+except FileNotFoundError:
+    pass
+
+lst = list(map(int, input().split(" ")))
+num = lst[0]
+n = lst[1]
+last = lst[2]
+
+def divide_conquer(num, n, last):
+    if n == 1:
+        return (num ** n) % last
+    
+    temp = divide_conquer(num, n // 2, last)
+    
+    if n % 2 == 0:
+        return (temp * temp) % last
+    else:
+        return (temp * temp * num) % last
+
+print(divide_conquer(num, n, last))
+
+
+
+
+
