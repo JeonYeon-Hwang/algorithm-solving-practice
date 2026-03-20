69596d7a821c0bd8e71f130ef54055f386da56e9
JeongYeonH <ubitnoa@gmail.com>
2026-03-20 22:27:02 +0900

    하 + 2 문제 풂

--- week4/problem-solving/난이도하_트리_상근이의여행_실버4.py
+++ week4/problem-solving/난이도하_트리_상근이의여행_실버4.py
@@ -1,2 +1,29 @@
 # 트리 - 상근이의 여행 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/9372
+import sys
+
+try:
+    sys.stdin = open('난이도하_트리_상근이의여행_실버4.txt', 'r')
+except FileNotFoundError:
+    pass
+
+
+T = int(input().rstrip())
+
+
+input = sys.stdin.readline
+
+case_idx = 0
+
+while True: 
+    nums = input().rstrip() 
+    
+    if nums == "":
+        break
+    temp = list(map(int, nums.split(' ')))
+    
+    if case_idx == 0:
+        print(temp[0] - 1)
+        case_idx = temp[1]
+    else:
+        case_idx -= 1
--- week4/problem-solving/난이도하_트리_트리만들기_실버4.py
+++ week4/problem-solving/난이도하_트리_트리만들기_실버4.py
@@ -1,2 +1,26 @@
 # 트리 - 트리 만들기 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/14244
+import sys
+
+try:
+    sys.stdin = open('난이도하_트리_트리만들기_실버4.txt', 'r')
+except FileNotFoundError:
+    pass
+
+input = sys.stdin.readline
+n, m = map(int, input().rstrip().split(' '))
+lst = [ 1 for _ in range(n)]
+
+lst[-m] = m
+for i in range(n - (m + 1), 0 , -1):
+    lst[i] = 2
+
+print('0 1')
+for i in range(1, len(lst)):
+    how_many = lst[i]
+
+    move_left = i
+    while how_many > 1:
+        how_many -= 1
+        move_left += 1
+        print(f'{i} {move_left}')
