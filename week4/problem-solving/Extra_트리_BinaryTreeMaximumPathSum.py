0520977dcf28d253a30ebcd8c93729911d4947a5
JeongYeonH <ubitnoa@gmail.com>
2026-03-25 09:15:19 +0900

    leetcode 어려움에 속한다고 하나, 이진트리와 값 계산 방향에 대해 파악하면 접근 가능함

--- week4/problem-solving/Extra_트리_BinaryTreeMaximumPathSum.py
+++ week4/problem-solving/Extra_트리_BinaryTreeMaximumPathSum.py
@@ -1,2 +1,58 @@
 # 트리 - Binary Tree Maximum Path Sum
 # 문제 링크: https://leetcode.com/problems/binary-tree-maximum-path-sum/?envType=study-plan-v2&envId=top-interview-150
+import math
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+
+
+class Solution:
+    def __init__(self):
+        self.max_sum = -math.inf
+
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:    
+        self.dfs_sum(root)
+        return self.max_sum
+
+    def dfs_sum(self, node):
+        if node.left is None and node.right is None:
+            self.max_sum = max(self.max_sum, node.val)
+            return node.val
+
+        max_l = 0
+        max_r = 0
+
+        if node.left is not None:
+            max_l = self.dfs_sum(node.left)
+        if node.right is not None:
+            max_r = self.dfs_sum(node.right)
+        
+        cutoff_sum = max_l + max_r + node.val
+        self.max_sum = max(self.max_sum, cutoff_sum, node.val)
+        
+        push_up_val = max(node.val, node.val + max_l, node.val + max_r)
+        self.max_sum = max(self.max_sum, push_up_val)
+        
+        return push_up_val
+    
+
+
+if __name__ == "__main__":
+    root = TreeNode(-1)
+
+    root.right = TreeNode(9)
+    root.right.left = TreeNode(-6)
+    root.right.right = TreeNode(3)
+
+    root.right.right.right = TreeNode(-2)
+
+    sol = Solution()
+    result = sol.maxPathSum(root)
+    
+    print(f"결과: {result}")
--- week4/problem-solving/Extra_트리_ImplementTriePrefixTree.py
+++ week4/problem-solving/Extra_트리_ImplementTriePrefixTree.py
@@ -1,2 +1,31 @@
 # 트리 - Implement Trie (Prefix Tree)
 # 문제 링크: https://leetcode.com/problems/implement-trie-prefix-tree/?envType=study-plan-v2&envId=top-interview-150
+
+
+from typing import Optional
+
+
+class TreeNode:
+     def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+
+
+
+        return 1
+    
+
+
+
+if __name__ == "__main__":
+    root = [-10, 9, 20, None, None, 15, 7]
+    
+    sol = Solution()
+    result = sol.maxPathSum(root)
+    
+    print(f"결과: {result}")
