21e5d5b830f2d9469a3b752ee0c216747b4ad81e
JeongYeonH <ubitnoa@gmail.com>
2026-03-28 00:23:46 +0900

    미 추적 파일 추가 => basic 및 lecture

--- week5/basic/01_dp_fibonacci.py
+++ week5/basic/01_dp_fibonacci.py
@@ -84,17 +84,26 @@ def fibonacci_memo(n, memo=None):
         n번째 피보나치 수
     """
     # TODO: memo가 None이면 빈 딕셔너리로 초기화
+    if memo == None:
+        memo = {}
     pass
     
     # TODO: base case 
+    memo[0] = 0
+    memo[1] = 1
     pass
     
     # TODO: 이미 계산한 값이 memo에 있으면 반환
+    if n in memo:
+        return memo[n]
     pass
     
     # TODO: 재귀 호출하여 계산하고 memo에 저장
+    for i in range(2, n + 1):
+        memo[i] = memo[i - 1] + memo[i - 2]
+
     pass
-    
+
     return memo[n]
 
 # 테스트 케이스
