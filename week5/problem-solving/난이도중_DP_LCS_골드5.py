2878e1cca04943a77ed4b6362f11a44da8f1bdb9
JeongYeonH <ubitnoa@gmail.com>
2026-03-28 13:33:26 +0900

    알고리즘 강의 보고 LCS 빠르게 풂

--- week5/problem-solving/난이도중_DP_LCS_골드5.py
+++ week5/problem-solving/난이도중_DP_LCS_골드5.py
@@ -1,2 +1,29 @@
 # DP - LCS (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/9251
+import sys
+from io import StringIO
+
+file = """ACAYKP
+CAPCAK"""
+
+
+sys.stdin = StringIO(file)
+
+input = sys.stdin.readline
+
+fst = input().rstrip()
+sec = input().rstrip()
+
+n = len(fst)
+m = len(sec)
+
+dp = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
+
+for i in range(1, n + 1):
+    for j in range(1, m + 1):
+        if fst[i - 1] == sec[j - 1]:
+            dp[i][j] = 1 + dp[i - 1][j - 1]
+        else:
+            dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
+
+print(dp[n][m])
--- week5/problem-solving/난이도중_DP_동전_골드5.py
+++ week5/problem-solving/난이도중_DP_동전_골드5.py
@@ -1,2 +1,34 @@
 # DP - 동전 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/9084
+import sys
+from io import StringIO
+
+file = """3
+2
+1 2
+1000
+3
+1 5 10
+100
+2
+5 7
+22"""
+
+
+sys.stdin = StringIO(file)
+
+input = sys.stdin.readline
+T = int(input())
+
+# dp에 대한 감을 잡았으나, 중복 처리를 하는 것이 문제...
+
+
+for _ in range(T):
+    N = int(input())
+    lst = list(map(int, input().split(' ')))
+    M = int(input())
+
+    print(f'{N}, {lst}, {M}')
+
+
+
