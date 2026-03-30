3d85ea9819aac0c9780889df84bb597a13fc6218
JeongYeonH <ubitnoa@gmail.com>
2026-03-30 14:48:12 +0900

    예외케이스를 살펴볼 것

--- week5/problem-solving/난이도중_그리디_회의실배정_골드5.py
+++ week5/problem-solving/난이도중_그리디_회의실배정_골드5.py
@@ -1,2 +1,39 @@
 # 그리디 - 회의실 배정 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/1931
+import sys
+from io import StringIO
+
+file = """11
+1 4
+3 5
+0 6
+5 7
+3 8
+5 9
+6 10
+8 11
+8 12
+2 13
+12 14
+"""
+
+sys.stdin = StringIO(file)
+input = sys.stdin.readline
+
+N = int(input())
+lst = []
+
+for _ in range(N):
+    start, end = map(int, input().split(' '))
+    lst.append((start, end))
+
+lst.sort(key=lambda x: (x[1], x[0]))
+# print(lst)
+selected = []
+selected.append(lst[0])
+
+for i in range(1, len(lst)):
+    if selected[-1][1] <= lst[i][0]:
+        selected.append(lst[i])
+
+print(len(selected))
