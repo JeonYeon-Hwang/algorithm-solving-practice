05f407537ed3f6fb043f9e975d6cd3fdf2fc2dcd
JeongYeonH <ubitnoa@gmail.com>
2026-03-31 15:38:45 +0900

    기본 구절 저장

--- week5/problem-solving/난이도상_DP_외판원순회_골드1.py
+++ week5/problem-solving/난이도상_DP_외판원순회_골드1.py
@@ -1,2 +1,24 @@
 # DP - 외판원 순회 (백준 골드1)
 # 문제 링크: https://www.acmicpc.net/problem/2098
+import sys
+from io import StringIO
+
+file = """4
+0 10 15 20
+5 0 9 10
+6 13 0 12
+8 8 9 0
+"""
+
+sys.stdin = StringIO(file)
+input = sys.stdin.readline
+
+N = int(input())
+arr = []
+
+for i in range(N):
+    row = list(map(int, input().split(' ')))
+    arr.append(row)
+
+
+print(arr)
